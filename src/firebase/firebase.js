ceef8db2232d93237328f80da6e5bec7e63397d9
Roger Ximenez <rogerximenez@gmail.com>
2024-06-06 19:58:44 -0500

    setup test database environments

---
+++ src/firebase/firebase.js
@@ -0,0 +1,144 @@
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = [];
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       })
+//     })
+
+//     console.log(expenses);
+//   })
+
+// database.ref('expenses')
+//   .on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+//   })
+
+// database.ref('expenses')
+//   .on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+//   })
+
+// database.ref('expenses')
+//   .on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+//   })
+
+  // database.ref('expenses')
+  //   .on('value', (snapshot) => {
+  //     const expenses = [];
+  //     snapshot.forEach((childSnapshot) => {
+  //       expenses.push({
+  //         id: childSnapshot.key,
+  //         ...childSnapshot.val()
+  //       })
+  //     })
+  
+  //     console.log(expenses);
+  // }, (e) => {
+  //   console.log('Error with data fetching', e)
+  // }) //sets up a subscription
+
+// database.ref('expenses').push({
+//   description: 'Gum',
+//   note: '',
+//   amount: 195,
+//   createdAt: 0
+// });
+
+// database.ref('notes').push({
+//   title: 'Course Topics Do',
+//   body: 'Go for a run'
+// })
+
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//   const val = snapshot.val();
+//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// }, (e) => {
+//   console.log('Error fetching data', e)
+// })
+
+// setTimeout(() => {
+//   database.ref().update({
+//     'job/title': 'Senior Product Manager',
+//     'job/company': 'Algolia',
+//     stressLevel: 2
+//   })
+// }, 3500);
+
+// database.ref('expenses').on('value', (snapshot) => {
+//   console.log(snapshot.val());
+// }, (e) => {
+//   console.log('Error with data fetching', e)
+// }) //sets up a subscription
+
+
+// setTimeout(() => {
+//   database.ref().off(onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//   database.ref('age').set(38);
+// }, 10500);
+
+// database.ref('location/city')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log('Error fetching data', e)
+//   })
+
+// database.ref('users').set({
+//   name: 'Roger Ximenez',
+//   age: 36,
+//   stressLevel: 6,
+//   job: {
+//     title: 'Director of Product',
+//     company: 'ID90 Travel'
+//   },
+//   location: {
+//     city: 'Dallas',
+//     country: 'United States'
+//   }
+// }).then(() => {
+//   console.log('Data is saved');
+// }).catch((e) => {
+//   console.log('This failed.', e);
+// })
+
+// database.ref().update({
+//   stressLevel: 9,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// });
+
+// database.ref('isSingle').remove().then(() => {
+//   console.log('Removed a column');
+// }).catch((e) => {
+//   console.log('Could not remove the data', e)
+// })
