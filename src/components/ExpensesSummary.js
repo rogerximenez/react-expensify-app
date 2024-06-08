536bf0fae7c6a911f69a7d40c2ac1d7dca9770b2
Roger Ximenez <rogerximenez@gmail.com>
2024-06-07 23:31:43 -0500

    added more styles

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from 'react-router-dom'
 import numeral from "numeral";
 import expensesTotal from "../selectors/expenses-total";
 import selectExpenses from '../selectors/expenses'
@@ -10,8 +11,13 @@ export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
   const expenseTotalFormatted = numeral(expensesTotal / 100).format('$0,0.00');
 
   return (
-    <div>
-      <h1>Viewing {expensesCount} {expenseWording} totaling {expenseTotalFormatted}</h1>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing <span>{expensesCount}</span> {expenseWording} totaling <span>{expenseTotalFormatted}</span></h1>
+        <div className="page-header__actions">
+          <Link className="btn-primary" to="/dashboard/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
   );
 };
