4f6669ed411613d9b73fd14989d60a757e2811bb
Roger Ximenez <rogerximenez@gmail.com>
2024-06-08 10:10:08 -0500

    finalize POC styling

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,16 +4,25 @@ import selectExpenses from '../selectors/expenses'
 import { connect } from "react-redux";
 
 export const ExpenseList = (props) => (
-  <div>
-    {
-      props.expenses.length === 0 ? (
-        <p>No expenses</p>
-      ) : (
-        props.expenses.map((expense) => {
-        return <ExpenseListItem key={expense.id} {...expense} />
-      })
-      )
-    }
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expense</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+    <div className="list-body">
+      {
+        props.expenses.length === 0 ? (
+          <div>
+            <span className="list-item list-item--message">No expenses</span>
+          </div>
+        ) : (
+          props.expenses.map((expense) => {
+          return <ExpenseListItem key={expense.id} {...expense} />
+        })
+        )
+      }
+    </div>
   </div>
 );
 
