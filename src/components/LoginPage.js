b1dcfd3814e8a21ff2b9c1cb683cdfd8d2ec4698
Roger Ximenez <rogerximenez@Rogers-MBP.lan>
2024-06-07 22:27:41 -0500

    add super small style changes

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,7 +6,7 @@ export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
       <h1 className="box-layout__title">Log In To Your Account</h1>
-      <p>Get your expenses under control.</p>
+      <p className="secondary-text">Get your expenses under control.</p>
       <button className="btn-primary" onClick={startLogin}>Login</button>
     </div>
   </div>
